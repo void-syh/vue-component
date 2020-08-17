490e7e725ea9dda6e9287e202d0588968c47f6f7
void-syh <1808095489@qq.com>
2020-08-17 21:34:20 +0800

    初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+      <div>
+          <circleLoading></circleLoading>
+      </div>
+  </div>
+</template>
+
+<script>
+import circleLoading from '../components/circleLoading'
+
+export default {
+    components:{
+        circleLoading
+    }
+}
+</script>
+
+<style>
+    .home{
+        display: grid;
+        grid-template-columns: repeat(2,1fr);
+        grid-template-rows: repeat(3,1fr);
+        height: 100vh;
+        align-items: center;
+        justify-items: center;
+        max-width: 800px;
+        margin: 0 auto;
+    }
+    .home > div{
+        transform: scale(1.5);
+    }
+</style>
